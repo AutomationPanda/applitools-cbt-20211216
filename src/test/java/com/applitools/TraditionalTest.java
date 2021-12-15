f91e9640cd00f8f600f8832e459f3082a9f437c9
Pandy Knight <andylpk.247@gmail.com>
2021-12-14 20:10:51 -0500

    Completed TraditionalTest assertions

--- src/test/java/com/applitools/TraditionalTest.java
+++ src/test/java/com/applitools/TraditionalTest.java
@@ -6,6 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TraditionalTest
 {
@@ -13,11 +21,13 @@ public class TraditionalTest
     private final String DEMO_SITE_CHANGED_URL = "https://demo.applitools.com/index_v2.html";
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeEach
     public void startWebDriver()
     {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 15);
     }
 
     @AfterEach
@@ -37,6 +47,11 @@ public class TraditionalTest
         verifyMainPage();
     }
 
+    private void waitForAppearance(By locator)
+    {
+        wait.until(d -> d.findElements(locator).size() > 0);
+    }
+
     private void loadLoginPage(boolean original)
     {
         driver.get(original ? DEMO_SITE_URL : DEMO_SITE_CHANGED_URL);
@@ -44,7 +59,11 @@ public class TraditionalTest
 
     private void verifyLoginPage()
     {
-
+        waitForAppearance(By.cssSelector("div.logo-w"));
+        waitForAppearance(By.id("username"));
+        waitForAppearance(By.id("password"));
+        waitForAppearance(By.id("log-in"));
+        waitForAppearance(By.cssSelector("input.form-check-input"));
     }
 
     private void performLogin()
@@ -56,6 +75,32 @@ public class TraditionalTest
 
     private void verifyMainPage()
     {
+        // Check various page elements
+        waitForAppearance(By.cssSelector("div.logo-w"));
+        waitForAppearance(By.cssSelector("div.element-search.autosuggest-search-activator > input"));
+        waitForAppearance(By.cssSelector("div.avatar-w img"));
+        waitForAppearance(By.cssSelector("ul.main-menu"));
+        waitForAppearance(By.xpath("//a/span[.='Add Account']"));
+        waitForAppearance(By.xpath("//a/span[.='Make Payment']"));
+        waitForAppearance(By.xpath("//a/span[.='View Statement']"));
+        waitForAppearance(By.xpath("//a/span[.='Request Increase']"));
+        waitForAppearance(By.xpath("//a/span[.='Pay Now']"));
 
+        // Check time message
+        assertTrue(Pattern.matches(
+                "Your nearest branch closes in:( \\d+[hms])+",
+                driver.findElement(By.id("time")).getText()));
+
+        // Check menu element names
+        var menuElements = driver.findElements(By.cssSelector("ul.main-menu li span"));
+        var menuItems = menuElements.stream().map(i -> i.getText().toLowerCase()).toList();
+        var expected = Arrays.asList("card types", "credit cards", "debit cards", "lending", "loans", "mortgages");
+        assertEquals(expected, menuItems);
+
+        // Check transaction statuses
+        var statusElements = driver.findElements(By.xpath("//td[./span[contains(@class, 'status-pill')]]/span[2]"));
+        var statusNames = statusElements.stream().map(n -> n.getText().toLowerCase()).toList();
+        var acceptableNames = Arrays.asList("complete", "pending", "declined");
+        assertTrue(acceptableNames.containsAll(statusNames));
     }
 }
